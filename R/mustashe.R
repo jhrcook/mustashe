20b1835c7a307d0899bc0eb62d20e6d6bd19ab7b
Josh Cook <joshuacook0023@gmail.com>
2020-03-21 07:52:44 -0400

    doc: R file for the package

---
+++ R/mustashe.R
@@ -0,0 +1,10 @@
+#' mustashe: Stash and Load Objects in R
+#'
+#' A simple system for saving and loading objects in R. Long running
+#' computations can be stashed after the first run and then reloaded the next
+#' time. Dependencies can be added to ensure that a computation is re-run if
+#' any of its dependencies or inputs have changed.
+#
+#' @docType package
+#' @name mustashe
+NULL
