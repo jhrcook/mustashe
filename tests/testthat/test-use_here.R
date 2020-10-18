944061ceb759aa6916885487a8ed2a3b7f1402f9
Josh Cook <joshuacook0023@gmail.com>
2020-10-18 11:33:26 -0400

    fix: don't use here in tests

--- tests/testthat/test-use_here.R
+++ tests/testthat/test-use_here.R
@@ -2,6 +2,8 @@ test_that("`stash()` uses the `here::here()` function", {
   expect_message(use_here(), "global option \"mustashe.here\" has been set `TRUE`")
   expect_silent(use_here(silent = TRUE))
   expect_true(getOption("mustashe.here"))
+
+  dont_use_here(silent = TRUE)
 })
 
 
@@ -9,4 +11,6 @@ test_that("`stash()` does not use the `here::here()` function", {
   expect_message(dont_use_here())
   expect_silent(dont_use_here(silent = TRUE))
   expect_false(getOption("mustashe.here"))
+
+  dont_use_here(silent = TRUE)
 })
