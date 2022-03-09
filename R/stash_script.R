2a6497a1ec8d17b68a9efd5a65aef517eb0388e7
Magnus Thor Torfason <m@zulutime.net>
2022-03-09 15:09:22 +0000

    feat: implement stash_script (stash results of sourcing a script)

---
+++ R/stash_script.R
@@ -0,0 +1,54 @@
+
+
+
+#' Source script and stash result
+#'
+#' @description
+#' Source R script, only if the result has not already been stashed. The final
+#' expression in the script is treated as the return value from the script (as
+#' per the functionality of \code{source()}, and this value is returned and
+#' stashed (using  \code{stash()} as the underlying caching mechanism).
+#'
+#' @param script_path Path (absolute or relative) to the script to source.
+#' @param verbose Whether to print action statements (default TRUE).
+#'
+#' @return The last line in the script referred to by the script at
+#'   \code{script_path} is treated as a return value, and is returned and cached.
+#'
+#' @details
+#' This function always calls \code{stash()} in functional mode.
+#'
+#' Note that the stashed result is keyed according to the specified path to the
+#' script without any canonization. The function will therefore not realize that
+#' an absolute and relative path refer to the same file, and treat them as two
+#' separate results to cache.
+#'
+#' @examples
+#' script_name <- tempfile()
+#' set.seed(42)
+#' write("sample(letters,5)", script_name)
+#' x <- stash_script(script_name) # will be cached
+#' print(x)
+#' x <- stash_script(script_name) # will use cached data
+#' print(x)
+#'
+#' @export
+stash_script <- function(script_path, verbose = TRUE)  {
+
+  # check input
+  if ( is.null(script_path) ||
+       length(script_path) != 1 ||
+       !file_test("-f", script_path)) {
+    stop("script name invalid or missing")
+  }
+
+  # the stash depends_on mtime and contents of script_path
+  script_key  <- list("stash_script()",script_path)
+  script_deps <- list(file.mtime(script_path),
+    digest::digest(script_path, file=TRUE))
+
+  # Use stash to source the script, and return the result
+  stash(script_key, { source(file = script_path, local = TRUE)$value },
+        depends_on = "script_deps", functional = TRUE, verbose  = verbose)
+
+}
