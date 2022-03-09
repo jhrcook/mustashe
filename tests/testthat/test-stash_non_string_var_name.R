b2cc010703e2cc388898dfc9b99d903a7cc90a6d
Magnus Thor Torfason <m@zulutime.net>
2022-03-09 14:56:32 +0000

    test: add tests of arbitrary objects as stash key (var parameter)

---
+++ tests/testthat/test-stash_non_string_var_name.R
@@ -0,0 +1,25 @@
+
+
+test_that("non-string var names work if functional is specified", {
+
+  # make.names(c("good_var_name","bad-var-name", "bad/file/name"))
+
+  # using NULL as var always results in an error
+  expect_error(stash(NULL, {1}))
+  expect_error(stash(NULL, {1}, functional=TRUE))
+
+  # in regular use, good variable names should be accepted and bad ones rejected
+  expect_null(stash("good_var_name",  {1}))
+  expect_error(stash("bad-var-name",  {1}))
+  expect_error(stash("bad/file/name", {1}))
+  expect_error(stash(c("s_1", "s_2"), {1}))
+  expect_error(stash(list(letters, cars), {1}))
+
+  # in functional use, arbitrary objects should be accepted as names/keys
+  expect_equal(stash("good_var_name", {1}, functional=TRUE), 1)
+  expect_equal(stash("bad-var-name",  {1}, functional=TRUE), 1)
+  expect_equal(stash("bad/file/name", {1}, functional=TRUE), 1)
+  expect_equal(stash(c("s_1", "s_2"), {1}, functional=TRUE), 1)
+  expect_equal(stash(list(letters, cars), {1}, functional=TRUE), 1)
+
+})
