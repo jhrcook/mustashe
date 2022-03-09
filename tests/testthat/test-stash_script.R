368a78c5c3631bf6a8091e26ea7bd89e605e2974
Magnus Thor Torfason <m@zulutime.net>
2022-03-09 15:44:29 +0000

    test: add tests for using depends_on with stash_script

--- tests/testthat/test-stash_script.R
+++ tests/testthat/test-stash_script.R
@@ -21,3 +21,26 @@ test_that("stash_script works", {
   expect_equal(x, "e")
 
 })
+
+test_that("stash_script correctly handles depends_on", {
+
+  # init
+  set.seed(42) # sampled sequence: "q" "e" "a" "y" "j" "d" ...
+  script_name <- tempfile()
+  write("sample(letters,1)", script_name)
+
+  # will be stashed
+  the_dependency <- "initial value"
+  x <- stash_script(script_name, depends_on = "the_dependency")
+  expect_equal(x, "q")
+
+  # will be retrieved
+  the_dependency <- "initial value"
+  x <- stash_script(script_name, depends_on = "the_dependency")
+  expect_equal(x, "q")
+
+  # changing file causes restash
+  the_dependency <- "new value"
+  x <- stash_script(script_name, depends_on = "the_dependency")
+  expect_equal(x, "e")
+})
